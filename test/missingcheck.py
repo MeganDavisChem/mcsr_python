20ad31bd23a0d9694494b69487a0d2f0173b3cbe
Megan Davis <mdavis@master.cl.olemiss.edu>
2022-02-08 13:56:21 -0600

    a

--- test/missingcheck.py
+++ test/missingcheck.py
@@ -14,34 +14,38 @@ qffSize = int(argv[2])
 miss = 1
 missing = False
 missing_numbers = []
+print("???")
 
 #start searching through energies
 for i in range(0,len(energies)):
     #set current number to iteration + number of missing points
+    print(i)
     j = i + miss
-    number = '{:04}.'.format(j)
+
+    if os.uname()[1] == 'sequoia':
+        number = '{:04}/'.format(j)
+    else:
+        number = '{:04}.'.format(j)
 
     #check if current number is missing
     if not (number in energies[i]):
         missing = True
 
     #iterate over missing points until one is found
-    print(number)
-    print(energies[i])
     while missing:
         miss = miss + 1
         missing_numbers.append(number)
         j = i + miss
-        number = '{:04}.'.format(j)
-        print('this is in while loop')
-        print(number)
-        print(energies[i])
+        if os.uname()[1] == 'sequoia':
+            number = '{:04}/'.format(j)
+        else:
+            number = '{:04}.'.format(j)
         #return to main loop once a number is found
         if number in energies[i]:
             missing = False
 
 #check for final point
-if not ('.{:04}.'.format(qffSize) in energies[len(energies)-1]):
+if not ('{:04}.'.format(qffSize) in energies[len(energies)-1]):
     missing_numbers.append('{:04}.'.format(qffSize))
 
 #write submit script for eland or mcsr
@@ -51,6 +55,9 @@ f = open(missingscript,"w")
 if os.uname()[1] == 'master':
     for num in missing_numbers:
         f.write("sbatch *"+num+'sh\n')
+elif os.uname()[1] == 'sequoia':
+    for num in missing_numbers:
+        f.write("(cd "+num+" && qsub ZMAT.pbs)\n")
 else:
     for num in missing_numbers:
         f.write("qsub *"+num+'pbs\n')
