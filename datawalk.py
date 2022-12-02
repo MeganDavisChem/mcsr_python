b0604fd01dcbebdf9b6b62f9c803c68996c00f59
Megan Davis <mdavis@master.cl.olemiss.edu>
2022-12-02 17:46:58 -0600

    sketch for datawalk

--- datawalk.py
+++ datawalk.py
@@ -9,21 +9,50 @@ import subprocess
 # import sys
 
 
+
+
 def main():
     """Locate spectro2.out files in subdirs,
     runs rsummarize on each then prints a summary of remaining work to be done
     spectro2.out files are assumed to all be located at the same level of depth
     for summary.txt to be accurate"""
     # TODO allow choosing input directory
-    # TODO form composite tables? Or use my own stuff to do that idk
     completed, incompleted = check_completed()
     rsum_options = ["json", "csv", "tex"]
 
+    # form individual tables
     for spec_dir in completed:
-        rsum_outputs = run_rsummarize(spec_dir, rsum_options)
-        write_rsums(spec_dir, rsum_outputs)
+        rsum_outputs_sep = run_rsummarize([spec_dir], rsum_options)
+        write_rsums(spec_dir, rsum_outputs_sep)
 
     write_summary()
+
+    # form composite tables
+    rsum_outputs = run_rsummarize(completed, rsum_options)
+    write_rsums(".", rsum_outputs, "combined")
+
+    #TODO
+    panda_tables = parse_into_pandas(rsum_outputs)
+    pandas_tables = insert_experiment(panda_tables)
+    fixed_panda_tables = fix_pandas_tables(completed, panda_tables)
+    write_pandas(fixed_pandas_tables)
+
+def parse_into_pandas(rsum_outputs):
+    """TODO read either csv or json into a pandas dataframe"""
+    pass
+
+def insert_experiment(pandas_tables):
+    """TODO allow inserting experimental values from a plaintext file?"""
+    pass
+
+def fix_pandas_tables(dir_list, panda_tables):
+    """TODO fix headers with proper names of molecules, format for LaTex etc"""
+    pass
+
+def write_pandas(fixed_pandas_tables):
+    """TODO write pandas dataframe to organized file(s)"""
+    pass
+
 
 
 def write_summary(directory: str = "."):
@@ -41,6 +70,12 @@ def write_summary(directory: str = "."):
             "Good progress!\n",
             "Get to work!\n",
             "Don't stop now!\n",
+            "You can do it!\n",
+            "Work hard, play hard :)\n",
+            "Courage is the magic that turns dreams into reality!\n",
+            "Little rough, don't ya think?\n",
+            "Eyes on the prize!\n",
+            "One step at a time!\n",
         ]
         return random.choice(motivationals)
 
@@ -60,35 +95,38 @@ def write_summary(directory: str = "."):
         file.write(incompleted_str)
 
 
-def write_rsums(spec_dir: str, rsum_outputs: dict):
+def write_rsums(spec_dir: str, rsum_outputs: dict, header: str = "spectro2"):
     """Runs rustsummarize and outputs files.
     Actually I guess I should decompose this function"""
     for flag, output in rsum_outputs.items():
-        filename = "spectro2." + flag
+        filename = header + "." + flag
         filepath = join(spec_dir, filename)
         with open(filepath, "w") as file:
             file.write(output)
 
 
-def run_rsummarize(completed_dir: str, options: list = ["json"]):
-    """Runs rsummarize and gives outputs as a dict for each completed dir"""
+def run_rsummarize(completed_dirs: list, options: list = ["json"]):
+    """Runs rsummarize with all files grouped together into a giant data table.
+    Run at toplevel of a species directory
+
+    Since this is almost exactly the same as below, I should probably delete the below one and just run this with a single input as a list
+    """
     rsum_outputs = {}
-    outfile = join(completed_dir, "spectro2.out")
+    outfiles = [join(completed_dir, "spectro2.out") for completed_dir in completed_dirs]
+
     for opt in options:
         out = subprocess.run(
-            ["rsummarize", f"--{opt}", outfile], capture_output=True, check=False
+            ["rsummarize", f"--{opt}"] + outfiles, capture_output=True, check=False
         ).stdout.decode()
         rsum_outputs[opt] = out
 
-    # Do a plain run
     opt = "sum"
     out = subprocess.run(
-        ["rsummarize", outfile], capture_output=True, check=False
+        ["rsummarize"] + outfiles, capture_output=True, check=False
     ).stdout.decode()
     rsum_outputs[opt] = out
-    # dirs_and_outputs = zip(completed_dirs, rsums)
+
     return rsum_outputs
-    # return dirs_and_outputs
 
 
 def check_completed(target_dir: str = "."):
@@ -102,7 +140,6 @@ def check_completed(target_dir: str = "."):
 
     completed = []
     incompleted = []
-    print("what")
     for root, dirs, files in os.walk(target_dir):
         if "spectro2.out" in files:
             completed.append(root)
